a0a48c6bb8fd84a1f249f3f4dbfbde760072b22a
Aswanth Vc <aswanth.abam@gmail.com>
2024-06-18 17:41:15 +0530

    fix: handle drop leave events

--- app/page.tsx
+++ app/page.tsx
@@ -18,8 +18,6 @@ export default function Home() {
 
   const mainDragOver = (event: React.DragEvent) => {
     event.preventDefault();
-    document.body.style.setProperty("--x", `${event.clientX}px`);
-    document.body.style.setProperty("--y", `${event.clientY}px`);
     document.body.style.setProperty(
       "--cursor-colors",
       `radial-gradient(
@@ -28,15 +26,30 @@ export default function Home() {
       rgba(175, 224, 228, 1)
     )`
     );
+    document.querySelector("#bg")?.classList.add(styles.active);
+    document.querySelector("#dropZone")?.classList.add(styles.active);
   };
 
   const mainDrop = (event: React.DragEvent) => {
-      event.preventDefault();
-      if(event.dataTransfer.files) {
-        alert(`File dropped ${event.dataTransfer.files[0].name}`);
-        
-      }
-  }
+    event.preventDefault();
+    mainDragEnd();
+    if (event.dataTransfer.files) {
+      alert(`File dropped ${event.dataTransfer.files[0].name}`);
+    }
+  };
+
+  const mainDragEnd = () => {
+    document.body.style.setProperty(
+      "--cursor-colors",
+      `radial-gradient(
+        ellipse at var(--x) var(--y),
+        rgba(142, 247, 182, 1) 0%,
+        rgba(175, 224, 228, 1)
+      )`
+    );
+    document.querySelector("#bg")?.classList.remove(styles.active);
+    document.querySelector("#dropZone")?.classList.remove(styles.active);
+  };
 
   useEffect(() => {
     document.body.style.setProperty("--x", `50%`);
@@ -45,9 +58,13 @@ export default function Home() {
 
   return (
     <main
-      onDragOver={mainDragOver}
-      onDrop={mainDrop}
       id="main"
+      onDragOver={mainDragOver}
+      onDragLeave={(e: React.DragEvent) => {
+        if (!document.querySelector("#main")?.contains(e.relatedTarget as Node))
+          mainDragEnd();
+      }}
+      onDrop={mainDrop}
       onMouseMove={mainMouseMove}
       className={styles.main}
     >
@@ -76,6 +93,20 @@ export default function Home() {
         </div>
       </div>
       <div id="bg" className={styles.bg}></div>
+      <div id="dropZone" className={styles.dropZone}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          className={styles.icon}
+          viewBox="0 0 16 16"
+        >
+          <path d="M8.5 11.5a.5.5 0 0 1-1 0V7.707L6.354 8.854a.5.5 0 1 1-.708-.708l2-2a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 7.707z" />
+          <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z" />
+        </svg>
+        <h1>Drop File to share</h1>
+      </div>
     </main>
   );
 }
