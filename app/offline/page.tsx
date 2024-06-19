f089c894c94d9f0922dccb0938a7d920310c9663
Aswanth Vc <aswanth.abam@gmail.com>
2024-06-19 19:52:04 +0530

    fix: Build Fix

--- app/offline/page.tsx
+++ app/offline/page.tsx
@@ -3,7 +3,7 @@ import styles from './page.module.css';
 export default function Offline() {
   return (
     <div className={styles.offline}>
-      <h1>Hmmm, We couldn't reach our server !!</h1>
+      <h1>Hmmm, We couldn&apos;t reach our server !!</h1>
       <p>It seems like our server is under maintainance. </p>
       <p><a href="https://aswanthvc.me/contact">In the mean time, donate us for making things better and faster</a></p>
     </div>
