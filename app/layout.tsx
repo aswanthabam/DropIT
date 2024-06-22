f29f45bdfbc60d91becaf81099bf33d87b2c3e1e
Aswanth Vc <aswanth.abam@gmail.com>
2024-06-22 17:51:05 +0530

    feat (seo): Added metatags and og images

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,29 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Topbar } from "@/components/Topbar";
 import MainContainer from "./main";
+import banner from '../public/banner.png';
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "DropIT",
-  description: "Share files easily.",
+  description: "DropIT is a fast and simple file-sharing platform. Share files without an account using a memorable code, ensuring easy and quick transfers across the internet.",
+  keywords: 'dropit, file sharing, instant file sharing, effortless file sharing, no account needed, simple file sharing, quick file transfer, DropIT, code-based file sharing, fast file sharing, secure file sharing',
+  authors: [{
+    name: 'Aswanth V C',
+    url: 'https://aswanthvc.me'
+  }],
+  openGraph: {
+    type: "website",
+    url: "https://drop-it.web.app",
+    title: "DropIT",
+    description: "DropIT is a fast and simple file-sharing platform. Share files without an account using a memorable code, ensuring easy and quick transfers across the internet.",
+    siteName: "DropIT",
+    images: [{
+      url: banner.src,
+    }],
+  },
+  twitter: { card: "summary_large_image", site: "@_aswanthvc", creator: "@_aswanthvc", "images": banner.src }
 };
 
 export default function RootLayout({
